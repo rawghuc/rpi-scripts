b5e6ff1bce09ec75bc61fe29bb9fd5665d56eafe
rawghuc <rawghuc@proton.me>
2025-12-25 15:26:49 +0530

    Create 08-install-python-req.sh

---
+++ 08-install-python-req.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+source ./00-common.sh
+
+RUN_USER="$1"
+
+REQ_FILE="/home/$RUN_USER/flaskserver/requirements.txt"
+log "Starting... with $RUN_USER"
+
+if [ -f "$REQ_FILE" ]; then
+  log "Installing Python dependencies..."
+  pip3 install -r "$REQ_FILE" --break-system-packages
+else
+  log "Failed..."
+  error "No requirements.txt found."
+fi
