1f915fc4fc568e3f495f427f4a755d75d6057dea
rawghuc <rawghuc@proton.me>
2025-12-25 15:26:13 +0530

    Create 07-clone-project.sh

---
+++ 07-clone-project.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+source ./00-common.sh
+
+RUN_USER="$1"
+REPO="git@github.com:Innoway-Technologies-OPC-Pvt-Ltd/rpiserver.git"
+BRANCH="deploy-rpi"
+
+log "Cloning repo..."
+
+su - $RUN_USER -c "
+  mkdir -p ~/flaskserver
+  cd ~/flaskserver
+  git clone $REPO . || true
+  git fetch origin
+  git checkout $BRANCH
+"
