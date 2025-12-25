fff703bdb8b542f7878c2290f5ccd6a717f73d20
rawghuc <rawghuc@proton.me>
2025-12-25 15:23:13 +0530

    Create 06-tailscale-setup.sh

---
+++ 06-tailscale-setup.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+source ./00-common.sh
+
+TS_KEY="$1"
+DEVICE_ID=$(cat /tmp/device_id)
+
+curl -fsSL https://tailscale.com/install.sh | sh
+
+log "Joining Tailscale..."
+tailscale up --auth-key "$TS_KEY" --hostname "$DEVICE_ID" || true
+
+sleep 2
+TS_IP=$(tailscale ip -4 | head -n1)
+echo "$TS_IP" >/tmp/tailscale_ip
+
+log "Tailscale IP: $TS_IP"
