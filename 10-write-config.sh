0756fca132d56cdd1349a1005b4ecfc09973267f
rawghuc <rawghuc@proton.me>
2025-12-27 12:44:43 +0530

    added customer related variables

--- 10-write-config.sh
+++ 10-write-config.sh
@@ -4,15 +4,17 @@ source ./00-common.sh
 DEVICE_ID=$(cat /tmp/device_id)
 DEVICE_NAME=$(cat /tmp/device_name)
 TS_IP=$(cat /tmp/tailscale_ip)
+RUN_USER="${SUDO_USER:-}"
 
 MQTT_USER=$(jq -r '.mqtt_username // "mqtt_user"' /tmp/backend_response.json)
 MQTT_PASS=$(jq -r '.mqtt_password // "mqtt_pass"' /tmp/backend_response.json)
 
+CUSTOMER=$(jq -r '.customer // ""' /tmp/backend_response.json)
 LOCATION=$(jq -r '.location // ""' /tmp/backend_response.json)
 DESCRIPTION=$(jq -r '.description // ""' /tmp/backend_response.json)
 SLOTS=$(jq -c '.slots // {}' /tmp/backend_response.json)
 
-cat >/home/pi/flaskserver/.env <<EOF
+cat >/home/$RUN_USER/flaskserver/.env <<EOF
 DEVICE_NAME=$DEVICE_NAME
 DEVICE_ID=$DEVICE_ID
 MQTT_USERNAME=$MQTT_USER
@@ -20,10 +22,10 @@ MQTT_PASSWORD=$MQTT_PASS
 TAILSCALE_IP=$TS_IP
 EOF
 
-cat >/home/pi/flaskserver/config.json <<EOF
+cat >/home/$RUN_USER/flaskserver/config.json <<EOF
 {
   "device_name": "$DEVICE_NAME",
-  "customer": "Innoway",
+  "customer": "$CUSTOMER",
   "status": "active",
   "location": "$LOCATION",
   "description": "$DESCRIPTION",
