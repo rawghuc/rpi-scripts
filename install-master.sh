8cc6b38848cb31eaef5c9bc5dab2bf32db29c19e
rawghuc <rawghuc@proton.me>
2025-12-27 12:57:45 +0530

    added echo statements and removed prompt variables

--- install-master.sh
+++ install-master.sh
@@ -8,28 +8,31 @@ SCRIPT_DIR="$(dirname "$0")"
 chmod +x "$SCRIPT_DIR"/*.sh
 
 echo "✔ Module scripts are now executable"
-
-# ---------------------------
-# Handle arguments
-# ---------------------------
-# DEPLOY_KEY="${1:-}"
-TAILSCALE_KEY="${2:-}"
-BACKEND_URL="${3:-}"
-
 echo "Starting Master Installer..."
 
 ./01-system-setup.sh
+echo "System setup, now disabling unwanted services..."
 ./02-disable-services.sh
-./03-install-deploy-key.sh "pi"
-./04-auto-static-ip.sh "192.168.1" "192.168.1.1" "8.8.8.8,1.1.1.1" 2 99
+echo "Disabled services, not installing deploy key..."
+./03-install-deploy-key.sh
+echo "Installed deploy key, now auto-assigning static ip..."
+./04-auto-static-ip.sh
+echo "Assigned static ip, now identifying device..."
 ./05-identify-device.sh
-./06-tailscale-setup.sh "$TAILSCALE_KEY"
-./07-clone-project.sh "pi" "git@github.com:Innoway-Technologies-OPC-Pvt-Ltd/rpiserver.git" "new-structure-v-1-1"
+echo "Identified device, now setting up tailscale..."
+./06-tailscale-setup.sh
+echo "Set up tailscale, now cloning project..."
+./07-clone-project.sh
+echo "Cloned project, now installing python requirements..."
 ./08-install-python-req.sh
-./09-register-backend.sh "$BACKEND_URL"
+echo "Installed python requirements, now registering backend..."
+./09-register-backend.sh
+echo "Registered backend, now writing config file..."
 ./10-write-config.sh
+echo "Written config file, now creating flask service..."
 ./11-create-flask-service.sh
+echo "Created flask service, now adding auto update service..."
 ./12-auto-update-service.sh
 
-echo "Done. Rebooting..."
+echo "Done. Now rebooting..."
 reboot
