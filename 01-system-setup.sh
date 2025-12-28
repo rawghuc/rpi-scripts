4c8656089ac522362a1d4c4d3eff252f4416dae6
rawghuc <rawghuc@proton.me>
2025-12-28 09:05:25 +0530

    using python virtual env

--- 01-system-setup.sh
+++ 01-system-setup.sh
@@ -4,7 +4,7 @@ source ./00-common.sh
 log "Updating system & installing base packages..."
 apt update && apt upgrade -y
 
-apt install -y git python3 python3-pip python3-dev build-essential curl jq network-manager
+apt install -y git python3 python3-pip python3-venv python3-dev build-essential curl jq network-manager
 
 log "Installing Mosquitto..."
 apt install -y mosquitto mosquitto-clients
