4165f1dd6a0c5103529b5aa0a527549f6a28bf70
rawghuc <rawghuc@proton.me>
2025-12-28 09:49:00 +0530

    using python virtual env

--- 11-create-flask-service.sh
+++ 11-create-flask-service.sh
@@ -8,13 +8,13 @@ log "Creating flaskserver service for user: $RUN_USER"
 
 cat >/etc/systemd/system/flaskserver.service <<EOF
 [Unit]
-Description=Innoway Flask Server
+Description=ParkSense Flask Server
 After=network.target
 
 [Service]
 Environment="PYTHONUNBUFFERED=1"
 WorkingDirectory=/home/${RUN_USER}/flaskserver
-ExecStart=/usr/bin/python3 /home/${RUN_USER}/flaskserver/app.py --host=0.0.0.0
+ExecStart=/home/${RUN_USER}/flaskserver/venv/bin/python /home/${RUN_USER}/flaskserver/app.py --host=0.0.0.0
 Restart=always
 RestartSec=5
 User=${RUN_USER}
@@ -24,6 +24,7 @@ StandardError=journal
 [Install]
 WantedBy=multi-user.target
 EOF
+
 
 log "Reloading systemd and starting flaskserver"
 
