9bd064fa83988b2fa48a0244103e85270c8b86bd
rawghuc <rawghuc@proton.me>
2026-01-12 00:54:45 +0530

    added right variables

--- 10-write-config.sh
+++ 10-write-config.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 source ./00-common.sh
+source /etc/cust_info/cust_info.env
 
 DEVICE_ID=$(cat /tmp/device_id)
 DEVICE_NAME=$(cat /tmp/device_name)
@@ -9,13 +10,15 @@ RUN_USER="${SUDO_USER:-}"
 MQTT_USER=$(jq -r '.mqtt_username // "mqtt_user"' /tmp/backend_response.json)
 MQTT_PASS=$(jq -r '.mqtt_password // "mqtt_pass"' /tmp/backend_response.json)
 
-CUSTOMER=$(jq -r '.customer // ""' /tmp/backend_response.json)
-LOCATION=$(jq -r '.location // ""' /tmp/backend_response.json)
-DESCRIPTION=$(jq -r '.description // ""' /tmp/backend_response.json)
+RPI_UUID=$(jq -r '.rpi_uuid // ""' /tmp/backend_response.json)
+
+# CUSTOMER=$(jq -r '.customer // ""' /tmp/backend_response.json)
+# LOCATION=$(jq -r '.location // ""' /tmp/backend_response.json)
+# DESCRIPTION=$(jq -r '.description // ""' /tmp/backend_response.json)
 SLOTS=$(jq -c '.slots // {}' /tmp/backend_response.json)
 
 cat >/home/$RUN_USER/flaskserver/.env <<EOF
-DEVICE_NAME=$DEVICE_NAME
+DEVICE_NAME=$RPI_NAME
 DEVICE_ID=$DEVICE_ID
 MQTT_USERNAME=$MQTT_USER
 MQTT_PASSWORD=$MQTT_PASS
@@ -24,12 +27,12 @@ EOF
 
 cat >/home/$RUN_USER/flaskserver/config.json <<EOF
 {
-  "device_name": "$DEVICE_NAME",
-  "customer": "$CUSTOMER",
+  "device_name": "$RPI_NAME",
+  "customer": "$CUST_ID",
   "status": "active",
   "location": "$LOCATION",
-  "description": "$DESCRIPTION",
-  "rpi_uuid": "$(get_serial)",
+  "description": "$RPI_DESC",
+  "rpi_uuid": "$RPI_UUID",
   "registered_park_slots": $SLOTS
 }
 EOF
